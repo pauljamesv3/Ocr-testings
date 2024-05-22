1e0effa68c1ad78823ab72b30982b24d177404ea
pauljamesv3 <pauljamesdavidson@v3analytics.in>
2024-05-22 15:54:32 +0530

    may 22 update

--- src/easy-ocr.py
+++ src/easy-ocr.py
@@ -10,8 +10,10 @@ for file_name in test_files:
     
     path = os.path.join('test', file_name)
 
+    start_time = time.time()
     result = reader.readtext(path)
-
+    end_time = time.time()
+    print(f'The time taken was{end_time - start_time}')
     text_results = [text for _, text, _ in result]
     print(f"{file_name}: ", text_results)
 
